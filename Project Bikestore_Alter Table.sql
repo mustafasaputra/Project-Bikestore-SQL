2cb8944f2d68cb04106a38da40fbb5feb857e2a3
mustafasaputra <mustafa.saputra19@gmail.com>
2025-04-24 00:39:01 +0700

    Update Project Bikestore_Alter Table.sql

--- Project Bikestore_Alter Table.sql
+++ Project Bikestore_Alter Table.sql
@@ -33,10 +33,3 @@ add constraint fk_sales_orders_order
 alter table sales_staffs
 add constraint fk_sales_staff_store
 	foreign key (store_id) references sales_stores (store_id) on delete cascade on update cascade;
-    
-alter table sales_orders
-rename column order_status_id to order_status;    
-    
-alter table sales_orders
-add constraint fk_order_status
-	foreign key (order_status_id) references order_status (order_status_id) on delete cascade on update cascade;
